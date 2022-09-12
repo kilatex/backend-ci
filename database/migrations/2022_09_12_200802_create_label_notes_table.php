90016b5e5994265c82c0de17a7a876b8a89f83d8
kilatex <santiagodmaldon@gmail.com>
2022-09-12 16:09:25 -0400

    fixing migrations

--- database/migrations/2022_09_12_200802_create_label_notes_table.php
+++ database/migrations/2022_09_12_200802_create_label_notes_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePivotLableNote extends Migration
+class CreateLabelNotesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreatePivotLableNote extends Migration
      */
     public function up()
     {
-        Schema::create('labelnotes', function (Blueprint $table) {
+        Schema::create('label_notes', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
             $table->unsignedBigInteger('label_id')->nullable();
@@ -38,6 +38,6 @@ class CreatePivotLableNote extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('pivot_lable_note');
+        Schema::dropIfExists('label_notes');
     }
 }
